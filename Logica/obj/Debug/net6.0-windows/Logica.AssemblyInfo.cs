8a4586db27f321f99f50a6cc15f9bb84e73b6ade
kevin <noriegakevin10@gmail.com>
2024-11-14 21:09:37 -0500

    Guardando Registro De Paciente

--- Logica/obj/Debug/net6.0-windows/Logica.AssemblyInfo.cs
+++ Logica/obj/Debug/net6.0-windows/Logica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530caef6da1e9f2ec73fdb1967e65d03f14984be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58cfb73498a1599419b2276b4bedf43351eeb23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
