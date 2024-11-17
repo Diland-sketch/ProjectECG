991ba3054216ed704c1ea1ca8928646ca4e9bcf6
kevin <noriegakevin10@gmail.com>
2024-11-16 23:52:53 -0500

    Ultimos cambios realizados

--- Logica/obj/Debug/net6.0-windows/Logica.AssemblyInfo.cs
+++ Logica/obj/Debug/net6.0-windows/Logica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93477c61bb45146c9ccb3246ea208402cda62449")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4a0d7ab9cca2a351a998f61ec4b26ba2b43605")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
