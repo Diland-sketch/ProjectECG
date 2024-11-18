3f43f75c5a35e8c80cd00255f34cc2adb60a2239
KerinM <kerinmindiola@gmail.com>
2024-11-18 01:10:50 -0500

    guardando cambios en vista monitoreo

--- GUI/View/LoginView.xaml.cs
+++ GUI/View/LoginView.xaml.cs
@@ -60,7 +60,7 @@ namespace GUI.View
             if (loginExitoso == 1)
             {
                 MessageBox.Show("Bienvenido de nuevo Admin");
-                viewAdmin viewAdmin = new viewAdmin();
+                DashBoardView viewAdmin = new DashBoardView();
                 viewAdmin.Show();
                 this.Close();
             }
