0e8c768dc6a3ae9cf7a3d6e11eb36737d72074d3
kevin <noriegakevin10@gmail.com>
2024-11-19 21:40:10 -0500

    jalando ultimos cambios

--- Logica/obj/Debug/net6.0-windows/Logica.AssemblyInfo.cs
+++ Logica/obj/Debug/net6.0-windows/Logica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f459a0800fce37ae9a7954524517b44095880ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac7645ee0fa2c54463e0e46e8d47cfab03692ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persitencia/obj/Debug/net6.0-windows/Persitencia.AssemblyInfo.cs
+++ Persitencia/obj/Debug/net6.0-windows/Persitencia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persitencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f459a0800fce37ae9a7954524517b44095880ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac7645ee0fa2c54463e0e46e8d47cfab03692ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persitencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persitencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
